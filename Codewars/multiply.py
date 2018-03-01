46b2763e9f826e3e703fd6ae44e7d84b56c7c522
Jemimah O <jemimahsummers@yahoo.com>
2018-03-01 23:43:58 +0100

    Add problem statement
    Also add a comment to indicate what was modified to make the code work

--- Codewars/multiply.py
+++ Codewars/multiply.py
@@ -1,4 +1,11 @@
 # link to challenge: https://www.codewars.com/kata/multiply/train/
 
+"""
+Problem Statement: 
+
+The code does not execute properly. Try to figure out why.
+"""
+
 def multiply(a, b):
+  # the original code that the problem statement references was missing the return keyword
   return a * b
